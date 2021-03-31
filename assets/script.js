a7286a98d4c88fc584f27b58886ed9e2289ad6e5
Longminh Do <longminhdo2000@gmail.com>
2021-03-31 15:41:41 +0700

    20210331 Homework

--- assets/script.js
+++ assets/script.js
@@ -1,164 +1,185 @@
-const DOMbtn1 = document.getElementById('btn1');
-const DOMbtn2 = document.getElementById('btn2');
-const DOMbtn3 = document.getElementById('btn3');
-const DOMbtn4 = document.getElementById('btn4');
-var letter = /[a-zA-Z]/;
+var data = []; // Array lưu thông tin object
+var showId = document.getElementById('id');
+var showName = document.getElementById('name');
+var showMail = document.getElementById('email');
+var showPhone = document.getElementById('phone');
+// check trùng id
+function checkID(id) {
+	var list = this.data;
+	for (var i = 0; i < list.length; i++) {
+		if (list[i].id == id) {
+			return true;
+		}
+	}
+}
+
+// Hiển thị
+function viewData() {
+	var list = this.data;
+	var table =
+		'<table border="1" cellpadding="5"><thead><tr><th>ID</th><th>Name</th><th>Email</th><th>Phone</th><th>Action</th></tr></thead><tbody>';
+
+
+	for (var i = 0; i < list.length; i++) {
+		table += '<tr>';
+		table += '<td>' + list[i].id + '</td>';
+		table += '<td>' + list[i].name + '</td>';
+		table += '<td>' + list[i].email + '</td>';
+		table += '<td>' + list[i].phone + '</td>';
+		table += '<td><button class = "button" onclick="deletePerson(' + i + ');">Delete</button></td>';
+		table += '</tr>';
+	}
+
+	table += '</tbody></table>';
+	document.getElementById('database').innerHTML = table;
+}
+
+// Thêm mới
+function addData() {
+	var id = document.getElementById('id').value;
+	var name = document.getElementById('name').value;
+	var email = document.getElementById('email').value;
+	var phone = document.getElementById('phone').value;
+
+	if (id == ''){
+		alert("Please fullfill these information");
+		resetScreen();
+		return;
+	}
+
+	if (name == ''){
+		alert("Please fullfill these information");
+		resetScreen();
+		return;
+	}
+
+	if (email == ''){
+		alert("Please fullfill these information");
+		resetScreen();
+		return;
+	}
+
+	if (phone == ''){
+		alert("Please fullfill these information");
+		resetScreen();
+		return;
+	}
 
 
 
-/**** EVEN OR ODD *********************************************/
+	var item = {
+		id: id,
+		name: name,
+		email: email,
+		phone: phone
+	};
 
-DOMbtn1.addEventListener("click", () => {
-    var input = parseInt(document.getElementById('input1').value);
-    var DOMresult = document.getElementById('result1');
+	if (!checkID(id)) {
+		this.data.push(item);
+		viewData();
+		alert('Add succesfully!');
+	} else {
+		alert('This ID has been existed');
+	}
 
-    if (!isNaN(input)) {
-        if ((input % 2) === 0) {
-            DOMresult.value = "This is an even number";
-        }
-        else {
-            DOMresult.value = "This is an odd number";
-        }
-    }
-    else {
-        DOMresult.value = "Invalid input!";
-    }
-});
+	resetScreen();
 
+}
 
+// Delete
+function deletePerson(index) {
+	if (confirm("Are you sure you want to delete this student")) {
+		// Xóa
+		data.splice(index, 1);
+		if ( this.data.length === 0){
+			clearScreen();
+		}
+		else{
+			viewData();
+		}
+	}
+}
 
-/**** STUDENT RANK ********************************************/
+//clearInput
+function resetScreen() {
+	//alert(123);
 
-DOMbtn2.addEventListener("click", () => {
-    var input = parseFloat(document.getElementById('input2').value);
-    var DOMresult = document.getElementById('result2');
-    var result;
+	showId.value = null;
+	showName.value = null;
+	showMail.value = null;
+	showPhone.value = null;
+}
 
-    if (input >= 0 && input <= 10) {
-        if (input < 5) {
-            result = "Weak";
-        }
-        else if (input >= 5 && input < 7) {
-            result = "Average";
-        }
-        else if (input >= 7 && input < 9) {
-            result = "Good";
-        }
-        else {
-            result = "Exellent";
-        }
-        DOMresult.value = `${result} student`;
-    }
-    else {
-        DOMresult.value = `Invalid input!`;
-    }
-});
+//search
+function search() {
+	var input = document.getElementById('id').value;
+	let flag = 0;
+	for (let i = 0; i < data.length; i++) {
+		if (input == data[i].id) {
+			//alert();
+			showId.value = data[i].id;
+			showName.value = data[i].name;
+			showMail.value = data[i].email;
+			showPhone.value = data[i].phone;
+			return;
+		}
+	}
+	if (flag == 0) {
+		alert("Input ID is not existed!");
+		resetScreen();
+	}
+	// alert();
+}
 
+//edit
+function saveData() {
+	var id = document.getElementById('id').value;
+	var name = document.getElementById('name').value;
+	var email = document.getElementById('email').value;
+	var phone = document.getElementById('phone').value;
 
+	// let item = {
+	// 	id: id,
+	// 	name: name,
+	// 	email: email,
+	// 	phone: phone
+	// };
+	//check wether ID existed
 
-/**** TIME CALCULATOR *****************************************/
-
-DOMbtn3.addEventListener("click", () => {
-    var DOMinput = document.getElementById('input3');
-    var unitLength = 1;
-    var unitTime = 1;
-    var road = parseFloat(DOMinput.value);
-    var DOMresult = document.getElementById('result3');
-    var result = 0;
-
-    switch (document.getElementById('unit-length').value) {
-        case "m":
-            unitLength = 1;
-            break;
-
-        case "km":
-            unitLength = 1000;
-            break;
-    }
-
-    switch (document.getElementById('unit-time').value) {
-        case "second":
-            unitTime = 1;
-            break;
-
-        case "minute":
-            unitTime = 60;
-            break;
-
-        case "hour":
-            unitTime = 3600;
-            break;
-    }
-
-    road *= unitLength;
-    if (!isNaN(road)) {
-        function timeCalc(s, time) {
-            console.log(`${road} - ${result}`);
-            if (road > 0) {
-                if (road >= s) {
-                    road -= s;
-                    result += (s * time);
-                }
-                else {
-                    result += (road * time);
-                    road -= road;
-                }
-            }
-        }
-
-        console.log(`---------------------`);
-        timeCalc(3000, 1);
-        timeCalc(2000, 2);
-        timeCalc(1000, 3);
-        timeCalc(Infinity, 5);
-        console.log(`${road} - ${result}`);
-        result /= unitTime;
-
-        DOMresult.value = result;
-    }
-    else {
-        DOMresult.value = "Invalid input!";
-    }
-
-});
+	if (id == ''){
+		alert("You have to search for the ID first");
+	}
+	else {
+		let index = traverseID(id);
+		if (index == -1){
+			alert("Input ID is not existed!");
+			resetScreen();
+			viewData();
+			return;
+		}
+		else {
+			data[index].id = id;
+			data[index].name = name;
+			data[index].email = email;
+			data[index].phone = phone;
+		}
+	}
+	resetScreen();
+	viewData();
+}
 
 
+function traverseID (input) {
+	for (let i = 0; i < data.length; i++){
+		if (input == data[i].id) {
+			return i;
+		}
+		else return -1;
+	}
+}
 
-/**** GRADE CONVERTOR *****************************************/
-
-DOMbtn4.addEventListener("click", () => {
-    var DOMinput = document.getElementById('input4');
-    var input = DOMinput.value;
-    var DOMresult = document.getElementById('result4');
-    var result;
-
-    if (input.match(letter)) {
-        input = DOMinput.value = input.toUpperCase();
-        switch (input) {
-            case "A":
-                result = 4;
-                break;
-
-            case "B":
-                result = 3;
-                break;
-
-            case "C":
-                result = 2;
-                break;
-
-            case "D":
-                result = 1;
-                break;
-
-            default:
-                result = "Out-schooled";
-                break;
-        }
-        DOMresult.value = result;
-    }
-    else {
-        DOMresult.value = "Invalid input!";
-    }
-
-});
+function clearScreen() {
+	if (this.data.length === 0) {
+		location.reload();
+	}
+}
