6523a180efbf79b1a0320b96a67d97038a68730f
Longminh Do <longminhdo2000@gmail.com>
2021-04-03 16:47:54 +0700

    20210403 HW

--- assets/script.js
+++ assets/script.js
@@ -1,185 +1,93 @@
-var data = []; // Array lưu thông tin object
-var showId = document.getElementById('id');
-var showName = document.getElementById('name');
-var showMail = document.getElementById('email');
-var showPhone = document.getElementById('phone');
-// check trùng id
-function checkID(id) {
-	var list = this.data;
-	for (var i = 0; i < list.length; i++) {
-		if (list[i].id == id) {
-			return true;
-		}
-	}
-}
-
-// Hiển thị
-function viewData() {
-	var list = this.data;
-	var table =
-		'<table border="1" cellpadding="5"><thead><tr><th>ID</th><th>Name</th><th>Email</th><th>Phone</th><th>Action</th></tr></thead><tbody>';
-
-
-	for (var i = 0; i < list.length; i++) {
-		table += '<tr>';
-		table += '<td>' + list[i].id + '</td>';
-		table += '<td>' + list[i].name + '</td>';
-		table += '<td>' + list[i].email + '</td>';
-		table += '<td>' + list[i].phone + '</td>';
-		table += '<td><button class = "button" onclick="deletePerson(' + i + ');">Delete</button></td>';
-		table += '</tr>';
-	}
-
-	table += '</tbody></table>';
-	document.getElementById('database').innerHTML = table;
-}
-
-// Thêm mới
-function addData() {
-	var id = document.getElementById('id').value;
+function registerMember() {
+	//nameValidation
+	// alert("longminhtest");
+	var flag = 0;
 	var name = document.getElementById('name').value;
-	var email = document.getElementById('email').value;
-	var phone = document.getElementById('phone').value;
+	var invalidName = document.getElementById('errorName');
+	var regName = /^[^\d+]*[\d+]{0}[^\d+]*$/;
 
-	if (id == ''){
-		alert("Please fullfill these information");
-		resetScreen();
-		return;
+	if (name == '' || name == null) {
+		invalidName.innerHTML = 'Please fill in this box!';
 	}
-
-	if (name == ''){
-		alert("Please fullfill these information");
-		resetScreen();
-		return;
-	}
-
-	if (email == ''){
-		alert("Please fullfill these information");
-		resetScreen();
-		return;
-	}
-
-	if (phone == ''){
-		alert("Please fullfill these information");
-		resetScreen();
-		return;
-	}
-
-
-
-	var item = {
-		id: id,
-		name: name,
-		email: email,
-		phone: phone
-	};
-
-	if (!checkID(id)) {
-		this.data.push(item);
-		viewData();
-		alert('Add succesfully!');
-	} else {
-		alert('This ID has been existed');
-	}
-
-	resetScreen();
-
-}
-
-// Delete
-function deletePerson(index) {
-	if (confirm("Are you sure you want to delete this student")) {
-		// Xóa
-		data.splice(index, 1);
-		if ( this.data.length === 0){
-			clearScreen();
-		}
-		else{
-			viewData();
-		}
-	}
-}
-
-//clearInput
-function resetScreen() {
-	//alert(123);
-
-	showId.value = null;
-	showName.value = null;
-	showMail.value = null;
-	showPhone.value = null;
-}
-
-//search
-function search() {
-	var input = document.getElementById('id').value;
-	let flag = 0;
-	for (let i = 0; i < data.length; i++) {
-		if (input == data[i].id) {
-			//alert();
-			showId.value = data[i].id;
-			showName.value = data[i].name;
-			showMail.value = data[i].email;
-			showPhone.value = data[i].phone;
-			return;
-		}
-	}
-	if (flag == 0) {
-		alert("Input ID is not existed!");
-		resetScreen();
-	}
-	// alert();
-}
-
-//edit
-function saveData() {
-	var id = document.getElementById('id').value;
-	var name = document.getElementById('name').value;
-	var email = document.getElementById('email').value;
-	var phone = document.getElementById('phone').value;
-
-	// let item = {
-	// 	id: id,
-	// 	name: name,
-	// 	email: email,
-	// 	phone: phone
-	// };
-	//check wether ID existed
-
-	if (id == ''){
-		alert("You have to search for the ID first");
+	else if (regName.test(name) == false) {
+		invalidName.innerHTML = 'Invalid name input!';
 	}
 	else {
-		let index = traverseID(id);
-		if (index == -1){
-			alert("Input ID is not existed!");
-			resetScreen();
-			viewData();
-			return;
-		}
-		else {
-			data[index].id = id;
-			data[index].name = name;
-			data[index].email = email;
-			data[index].phone = phone;
-		}
+		invalidName.innerHTML = '';
+		flag++;
 	}
-	resetScreen();
-	viewData();
-}
 
+	//phoneValidation
+	var phone = document.getElementById('phone').value;
+	var invalidPhone = document.getElementById('errorPhone');
+	var regGexPhone = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im;
 
-function traverseID (input) {
-	for (let i = 0; i < data.length; i++){
-		if (input == data[i].id) {
-			return i;
-		}
-		else return -1;
+	if (phone == '' || phone == null) {
+		invalidPhone.innerHTML = 'Please fill in this box!';
+	} else if (!regGexPhone.test(phone)) {
+		invalidPhone.innerHTML = 'Invalid phone number input!';
+		phone = '';
+	} else {
+		invalidPhone.innerHTML = '';
+		flag++;
 	}
-}
 
-function clearScreen() {
-	if (this.data.length === 0) {
-		location.reload();
+	//emailValidation
+	var regexEmail = /[A-Z0-9._%+-]+@[A-Z0-9-]+.+.[A-Z]{2,4}/igm;
+	var invalidEmail = document.getElementById('errorEmail');
+	var email = document.getElementById('email').value;
+	if (email == '' || email == null) {
+		invalidEmail.innerHTML = 'Please fill in this box! ';
 	}
+	else if (!regexEmail.test(email)) {
+		invalidEmail.innerHTML = 'Invalid email input! ';
+	}
+	else {
+		invalidEmail.innerHTML = '';
+		flag++;
+	}
+
+	// password Invalidation
+	var regexPass = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
+	var password = document.getElementById('pass').value;
+	var invalidPassword = document.getElementById('errorPass');
+	if (password == '' || pass == null) {
+		invalidPassword.innerHTML = 'Please fill in this box';
+	}
+	else if (!regexPass.test(password)) {
+		invalidPassword.innerHTML = 'Invalid password! ';
+	}
+	else {
+		invalidPassword.innerHTML = '';
+		flag++;
+	}
+
+	// check 
+	var passw = document.getElementById('passw').value;
+	var invalidConPass = document.getElementById('errorConPass');
+
+	if (passw == '' || passw == null) {
+		invalidConPass.innerHTML = 'Please fill in this box!';
+	} else if (password != passw) {
+		invalidConPass.innerHTML = 'Passwords are not matching!';
+	} else {
+		invalidConPass.innerHTML = '';
+		flag++;
+	}
+
+	if (flag ===5) {
+		var printName = document.getElementById('printName');
+		var printPhone = document.getElementById('printPhone');
+		var printEmail = document.getElementById('printEmail');
+		var printPass = document.getElementById('printPass');
+		var printPassw= document.getElementById('printPassw');
+
+		printName.value = name;
+		printPhone.value = phone;
+		printEmail.value = email;
+		printPass.value = password;
+		printPassw.value = passw;
+	}
+
+	return false;
 }
